cfde089af411de5d3c0d0a05e54255331e5d3908
terenceouma <doros.devs@gmail.com>
2024-07-01 14:16:07 +0300

    adds grade function in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const grade = prompt("What is your grade?")
 
+function letterGrade () {
+    
+    if (grade < 0 || grade > 100) {
+        alert(" Please input a number between 0 and 100")
+    } else if (grade < 40) {
+        alert("You got an E, better lack next time")
+    } else if (grade >= 40 && grade < 49) {
+        alert("You got a D, I bet if you study more you will do better")
+    } else if (grade >= 49 && grade <= 59 ) {
+        alert("You got a C, keep puching")
+    } else if (grade >= 60 && grade < 79) {
+        alert("You go a B, you are so close.")
+    } else if (grade >= 79) {
+        alert("WOW, you got an A")
+    }
+
+}
+
+letterGrade(grade);
